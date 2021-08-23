6484d61356f9d78592c3f9d1a742e758505ac430
danilo-cupido <danilocupido@gmail.com>
2021-08-23 15:45:02 +0100

    fix typo

--- Week-10/workshop/index.test.js
+++ Week-10/workshop/index.test.js
@@ -29,3 +29,19 @@ test('Can not subtract the numbers', () => {
   notEqual(calculate(3468, '-', 6486), -3018);
   notEqual(calculate(18.2, '-', 13.48), 4.72);
 });
+
+// MULTIPLICATION
+
+test('Can multiply the numbers', () => {
+  equal(calculate(1, '*', 1), 1, 'The result is correct');
+  equal(calculate(15, '*', -16), -240, 'It works properly');
+  equal(calculate(1000, '*', 3456), 3456000, 'Superb');
+  equal(calculate(18.2, '*', 13.48), 245.336, 'You got it right');
+});
+
+test('Can not multiply the numbers', () => {
+  notEqual(calculate(3, '*', 4), 12);
+  notEqual(calculate(15, '*', -16), -240);
+  notEqual(calculate(1000, '*', 3456), 3456000);
+  notEqual(calculate(18.2, '*', 13.48), 245.336);
+});
